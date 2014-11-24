93dc6e4533fec4470b31b372f192a04871df20b6
Guillermo Correa <luis.guillermo.correa@hotmail.com>
2014-11-23 19:51:38 -0500

    initial commit

---
+++ BotserverDotNetClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quobject.SocketIoClientDotNet.Client;
+
+
+namespace BotserverDotNetClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = IO.Socket("https://guebot.herokuapp.com");
+            socket.On(Socket.EVENT_CONNECT, () =>
+            {
+                socket.Emit("movement", "data");
+                socket.On("movement", (data) =>
+                {
+                    Console.WriteLine(data);
+                });
+            });
+            Console.ReadLine();
+        }
+    }
+}
